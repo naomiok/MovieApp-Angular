ed85455af6ecae9b5bf3f404bdf3c62479889f4c
Carl Pacquing <cjpacquing@gmail.com>
2019-07-17 14:31:16 -0400

    New files added to Carl's branch.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { HomeComponent } from './components/home/home.component';
+import { WatchedComponent } from './components/watched/watched.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { ResetComponent } from './components/reset/reset.component';
+import { ViewProfileComponent } from './components/view-profile/view-profile.component';
+import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
+
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'register', component:RegisterUserComponent},
+  {path:'home', component:HomeComponent},
+  {path:'favorites', component:FavoritesComponent},
+  {path:'watched', component:WatchedComponent},
+  {path:'reset', component:ResetComponent},
+  {path:'view-profile', component:ViewProfileComponent},
+  {path:'update-profile', component:UpdateProfileComponent},
+  {path:'', redirectTo: 'login', pathMatch: 'full'}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+
+    id:number;
+    firstname:string;
+    lastname:string;
+    email:string;
+    password:string;
+    address:string;
+    city:string;
+    state:string;
+    phonenumber:string;
+    
+}
